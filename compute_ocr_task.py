0a6f1a7672402def3ebe517817ddb6197cd1a8a3
Félix <someonefefe@gmail.com>
2025-03-26 17:26:31 +0100

    adding support for tesseract

--- compute_ocr_task.py
+++ compute_ocr_task.py
@@ -4,6 +4,7 @@ import logging
 from typing import TYPE_CHECKING, Any
 
 import numpy as np
+import pytesseract
 
 if TYPE_CHECKING:
     from . import OCRPlugin
@@ -17,17 +18,20 @@ logger = logging.getLogger('PanopticOCR:OCRTask')
 
 
 class ComputeOCRTask(Task):
-    def __init__(self, plugin: OCRPlugin, instance: Instance, prop:Property):
+    def __init__(self, plugin: OCRPlugin, instance: Instance, prop:Property, model: str):
         super().__init__()
         self.plugin = plugin
         self.project = plugin.project
         self.instance = instance
         self.name = f'Compute OCR'
         self.prop = prop
+        self.model = model
 
     async def run(self):
         instance = self.instance
-        text = await self._async(self.make_ocr, instance.url)
+        ocr_function = self._get_ocr_function(self.model)
+        image = self.preprocess_image(instance.url)
+        text = await self._async(ocr_function, image)
         commit = DbCommit()
         commit.image_values.extend([ImageProperty(property_id=self.prop.id, sha1=instance.sha1, value=text)])
         res = await self.project.do(commit)
@@ -36,13 +40,7 @@ class ComputeOCRTask(Task):
         self.project._project.db.on_import_instance.emit(commit.image_values[0])
         return res.image_values[0]
 
-    def make_ocr(self, image_path):
-        # single_img_doc = DocumentFile.from_images(image_path)
-        image = Image.open(image_path)
-        params = self.plugin.params
-        if params.crop_x > 0:
-            x, y, w, h = params.crop_x, params.crop_y, params.crop_width, params.crop_height
-            image = image.crop((x, y, x + w, y + h))
+    def make_ocr_doctr(self, image: Image):
         image_arr = np.asarray(image)
         result = self.plugin._model([image_arr])
         res = ""
@@ -54,3 +52,25 @@ class ComputeOCRTask(Task):
                         res += " " + word['value']
         return res
 
+    def preprocess_image(self, image_path):
+        image = Image.open(image_path).convert('L')
+        params = self.plugin.params
+        x, y, w, h = params.crop_x, params.crop_y, params.crop_width, params.crop_height
+        if any([x > 0, y > 0, w > 0, h > 0]):
+            w = w if w != -1 else image.size[0]
+            h = h if h != -1 else image.size[1]
+            image = image.crop((x, y, x + w, y + h))
+        return image
+
+    def make_ocr_tesseract(self, image: Image):
+        custom_oem_psm_config = r'--oem 3 --psm 6'
+        return pytesseract.image_to_string(image, config=custom_oem_psm_config)
+
+    def _get_ocr_function(self, model):
+        match model:
+            case 'tesseract':
+                return self.make_ocr_tesseract
+            case 'doctr':
+                return self.make_ocr_doctr
+            case _:
+                return self.make_ocr_doctr
